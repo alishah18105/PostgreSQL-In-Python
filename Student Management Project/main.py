efda9793da229450a68383010c0752541849a964
alishah18105 <alishah18105@gmail.com>
2025-09-09 13:27:32 +0500

    Student Management Project Updated

--- Student Management Project/main.py
+++ Student Management Project/main.py
@@ -2,9 +2,23 @@
 import psycopg2
 import functions as f
 
-#f.create_Student_Table()
-# f.create_Course_Table()
-# f.create_Student_Info_Record_Table()
+print("\nWelcome To DCS Student Management System")
+print("========================================\n")
+
+mode = input("Are you a student or administor?\n(Enter 's' for student or 'a' for adminstrator ): ")
+
+if mode.lower() == 'a':
+    print("\nAdminstrator Dashboard:")
+    choice = int(input('''
+Press 1: To Add Student Record:
+Press 2: To Add Course Record:
+Press 3: To Add Student Information Record: ''')) 
+
+
+else:
+    print("\nStudent Dashboard\n")
+
+
 # f.insert_Course_Record()
 #f.insert_Student_Record()
-f.insert_Student_Info()
+#f.insert_Student_Info()
