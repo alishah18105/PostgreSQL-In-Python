af8f32aafed4f1ad5ca26362024bc8f802c4bfb3
alishah18105 <alishah18105@gmail.com>
2025-09-10 17:20:31 +0500

    Student Management System Project

--- Student Management Project/functions.py
+++ Student Management Project/functions.py
@@ -93,13 +93,28 @@ def insert_Course_Record():
     
 def insert_Student_Info():
     seat_number = input("\nEnter the student's seat number: ")
+    student_exist = get_Specific_Student_Details(seat_number)
+    if not student_exist:
+        print(f"\nNo student found with seat number: {seat_number} ")
+        choice = input(f"Do you want to add the student record for seat number \'{seat_number}\'? (Y/N): ")
+        if choice.lower() == 'y':
+            insert_Student_Record()
+
     semester = int(input("Enter the student's semester(eg: 1): "))
     records = get_Specific_Student_Record(seat_number,semester)
 
+    course_record = get_All_Course_Details()
+    all_course_list = [course_record[1] for course in course_record]
     if not records:
         for i in range(0,6):
             course_no = input("Enter the course_no you want to enter the marks of: ")
+            if course_no not in all_course_list:
+                print(f"\nCourse record with course no \'{course_no} doesn't exist ")
+                choice = input(f"Do you want to add the course record for course number \'{course_no}\'? (Y/N): ")
+                if choice.lower() == 'y':
+                    insert_Course_Record()
             marks = int(input(f"Enter the obtained marks of course {course_no}: "))
+            
             cur.execute(''' INSERT INTO student_info(seat_number,semester,course_no,marks)
                 VALUES
                 (%s,%s,%s,%s)
@@ -131,10 +146,11 @@ def insert_Student_Info():
 
 #---------------------------------------------------------------------------------------------------------
 
-def get_Specific_Student_Record(seat_num, sem):
-    cur.execute(''' SELECT course_no, marks FROM student_info
-                WHERE seat_number = %s AND semester = %s  
-                ORDER BY course_no
+def get_Specific_Student_Record(seat_num,sem):
+    cur.execute(''' SELECT st_in.course_no, c.credit_hours,st_in.marks FROM student_info st_in
+                    JOIN Course c ON st_in.course_no = c.course_no
+                    WHERE seat_number = %s AND semEster = %s
+                    ORDER BY course_id
             ''', (seat_num,sem)
             )
     rows = cur.fetchall() 
@@ -149,6 +165,22 @@ def get_Specific_Student_Details(seat_num):
     row = cur.fetchone()
     return row
 
+#---------------------------------------------------------------------------------------------------------
+def get_Specific_Course_Details(course_no):
+    cur.execute('''SELECT * FROM course
+                WHERE course_no = %s
+            ''', (course_no,)
+            )
+    row = cur.fetchone()
+    return row
+
+#---------------------------------------------------------------------------------------------------------
+def get_All_Course_Details():
+    cur.execute('''SELECT * FROM course ''',)
+    row = cur.fetchall()
+    return row
+
+
 #----------------------------------------------------------------------------------------------------------
 
 def update_Student_Info(marks, seat_number, semester, course_no):
@@ -157,6 +189,33 @@ def update_Student_Info(marks, seat_number, semester, course_no):
                 WHERE seat_number = %s AND semester = %s AND course_no = %s
                  ''', (marks, seat_number, semester, course_no))
     conn.commit()
+
+#------------------------------------------------------------------------------------------------------------
+def calculate_grading_point(marks):
+    if marks >= 90:
+        return ['A+', 4]
+    elif 85 <= marks <= 89:
+        return ['A', 4]
+    elif 80 <= marks <= 84:
+        return ['A-', 3.8]
+    elif 75 <= marks <= 79:
+        return ['B+', 3.4]
+    elif 71 <= marks <= 74:
+        return ['B', 3.0]
+    elif 68<=  marks <= 70:
+        return ['B-', 2.8]
+    elif 64 <= marks <= 67:
+        return ['C+', 2.4]
+    elif 61 <= marks <= 63:
+        return ['C', 2.0]
+    elif 57 <= marks <= 60:
+        return ['C-', 1.8]
+    elif 53 <= marks <= 56:
+        return ['D+', 1.4]
+    elif 50 <= marks <= 52:
+        return ['D-', 1.0]
+    else:
+        return ['F', 0.0]
     
 #-------------------------------------------------------------------------------------------------------------
 def close_connection():
--- Student Management Project/main.py
+++ Student Management Project/main.py
@@ -34,22 +34,49 @@ Press 3: To Add Student Information Record: '''))
 else:
     print("\n********************")
     print("Student Dashboard")
-    print("********************\n")
+    print("********************")
 
-    seat_number = input("Enter your seat number: ")
-    semester = input("Enter the semester: ")
-    student_detail = f.get_Specific_Student_Details(seat_number)
-    records = f.get_Specific_Student_Record(seat_number,semester)
+    choice = int(input('''
+Press 1: To View Semester Result:
+Press 2: To View Any Course Record:
+Press 3: To View All Courses Details: ''')) 
     
-    print("\n==================================================================")
-    print(f"\nName: {student_detail[1]}\t Father Name: {student_detail[2]}")
-    print(f"Seat No: {student_detail[3]}\t Program: {student_detail[4]}")
-    print(f"Semester: {semester}")
+    if choice == 1:
+        seat_number = input("Enter your seat number: ")
+        semester = input("Enter the semester: ")
+        student_detail = f.get_Specific_Student_Details(seat_number)
+        records = f.get_Specific_Student_Record(seat_number,semester)
+        
+        print("\n==================================================================")
+        print(f"\nName: {student_detail[1]}\t Father Name: {student_detail[2]}")
+        print(f"Seat No: {student_detail[3]}\t Program: {student_detail[4]}")
+        print(f"Semester: {semester}")
 
-    print("\n|Course Number \t| Marks |")
-    print("----------------------------")
-    for record in records:
-        print(f"|{record[0]} \t| {record[1]} \t|") 
-    print("\n==================================================================")
+        print("\n{:^13} | {:^15} | {:^13} |{:^14} |{:^14} |".format("Course No", "Credit Hours", "Obtained Marks", "Grade","GP"))
+        print("-"*85)
+        for record in records:
+            grade,gp = f.calculate_grading_point(record[2])
+            print("{:^13} | {:^15} | {:^14} | {:^13} | {:^13} |".format(record[0], record[1], record[2],grade,round((gp*int(record[1])),1)))
+        print("-"*85)
 
+
+
+    elif choice == 2:
+        course_no = input("\nEnter the course no: ")
+        course_record = f.get_Specific_Course_Details(course_no)
+        print("\n{:<12} | {:<60} | {:<13} |".format("Course No", "Course Title", "Credit Hours"))
+        print("-" * 90)  
+        print("{:<12} | {:<60} | {:^13} |".format(course_record[1], course_record[2], course_record[3]))
+    elif choice == 3:
+        course_records = f.get_All_Course_Details()
+
+        print("\n{:<12} | {:<60} | {:<13} |".format("Course No", "Course Title", "Credit Hours"))
+        print("-" * 93)  
+        for course_record in course_records:
+            print("{:<12} | {:<60} | {:^13} |".format(course_record[1], course_record[2], course_record[3]))
+        
+    else:
+        print("Invalid input entered")
+
+    
 
